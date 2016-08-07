cad33a1b26a5ce83b19a8276ca89e3454e1b6926
Alexander Kremer <alexanderkremer47@gmail.com>
2016-08-08 09:27:40 +1200

    Added website fonts and laid out basic website layout and content

---
+++ app/templates/home.php
@@ -0,0 +1,28 @@
+<?php 
+
+  $this->layout('master', [
+    'title'=>'Elite: Screenshots Homepage',
+    'desc'=>'The latest screenshots and art from the Elite: Dangerous community'
+  ]);
+
+?>
+
+
+<body id="home-page">
+
+  <div id="grid" class="large-12">
+
+    <article class="grid-item">
+      <a href="index.php?page=post">
+        <img src="img/test1.jpg" alt="">
+      </a>
+        <div class="upload-container">
+          <h1>
+              <a href="#">test</a>
+          </h1>
+          <a href="">CMDR test</a>
+        </div>
+
+    </article>
+
+  </div>
---
+++ app/templates/master.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?= $title ?> </title>
+    <meta name="description" content="<?= $desc ?>">
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href='https://fonts.googleapis.com/css?family=Sintony' rel='stylesheet' type='text/css'>
+    <script src="https://use.fontawesome.com/c9a6fcc362.js"></script>
+  </head>
+
+    <nav class="row expanded" data-sticky-container>
+    <div class="sticky" data-sticky data-margin-top="0">
+      <div class="columns large-4">
+        <a href="index.php?page=home"><img src="img/logo-test.png" alt=""></a>
+      </div>
+      <div class="columns large-4">
+        <div class="input-group">
+          <input type="search" name="search" placeholder="Search" class="input-group-field">
+            <div class="input-group-button">
+          <input type="submit" class="submit-button">
+        </div>
+      </div>
+        
+      </div>
+      <div class="columns large-4">
+        <ul>
+              <li><a href="index.php?page=logout">Log Out</a></li>
+              <li><a href="index.php?page=login">Login</a></li>
+              <li><a href="index.php?page=register">Register</a></li>
+              <li><a href="index.php?page=account">My Account</a></li>
+          </ul>
+      </div>
+      </div>
+    </nav>
+    
+    <?= $this->section('content') ?>
+
+
+    <footer class="row expanded">
+      <div class="columns large-8 large-centered">
+        <p> &copy; Alexander Kremer 2015-2016. This website was created using assets and imagery from Elite: Dangerous, with the permission of Frontier Developments plc, for non-commercial purposes. It is not endorsed by nor reflects the views or opinions of Frontier Developments and no employee of Frontier Developments was involved in the making of it.</p>
+      </div>
+    </footer>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
