533b303af764627f7dc2ea37514d08c91b86481a
Alexander Kremer <alexanderkremer47@gmail.com>
2016-08-11 11:50:43 +1200

    working on image uploading

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -20,69 +20,104 @@ class AccountController extends PageController {
 
 	public function buildHTML() {
 
-	
-
-		echo $this->plates->render('account', $this->data);
+	echo $this->plates->render('account', $this->data);
 
 	}
 
-	private function ProcessNewPost() {
+	private function ProcessNewUpload() {
 
 		$totalErrors = 0;
 
 		$title = trim($_POST['title']);
 		$desc  = trim($_POST['desc']);
 
-		$manager = new ImageManager(array('driver' => 'imagick'));
-
-		if( strlen( $title ) == 0 ) {
+		if ( strlen($title) == 0 ) {
 			$this->data['titleMessage'] = '<p>Required</p>';
 			$totalErrors++;
 		} elseif( strlen( $title ) > 50 ) {
-			$this->data['titleMessage'] = '<p>Cannot bemore than 50 characters</p>';
+			$this->data['titleMessage'] = '<p>Cannot be more than 50 characters</p>';
 			$totalErrors++;
 		}
 
 		if( strlen( $desc ) == 0 ) {
 			$this->data['descMessage'] = '<p>Required</p>';
 			$totalErrors++;
-		} elseif( strlen( $title ) > 400 ) {
+		} elseif( strlen( $desc ) > 400 ) {
 			$this->data['descMessage'] = '<p>Cannot bemore than 400 characters</p>';
 			$totalErrors++;
 		}
 
-		if( in_array( $_FILES['image']['error'], [1,3,4] ) ) {
-			$this->data['imageMessage'] = 'Image failed to upload';
-			$totalErrors++;
-		} elseif( !in_array( $_FILES['image']['type'], $this->acceptableImageTypes ) ) {
-			$this->data['imageMessage'] = 'Must be an image (jpg, gif, png, tiff etc)';
-			$totalErrors++;
+		if( $totalErrors == 0 ) {
+
+			$title = $this->dbc->real_escape_string($title);
+			$desc = $this->dbc->real_escape_string($desc);
+
+			$userID = $_SESSION['id'];
+
+			// $sql = "INSERT INTO uploads (title, description, user_id, image)
+			// 		VALUES ('$title', '$desc', $userID, '$fileName$fileExtension') ";
+
+			$sql = "INSERT INTO uploads (title, description, user_id)
+					VALUES ('$title', '$desc', $userID) ";
+
+			$this->dbc->query( $sql );
+
+			if( $this->dbc->affected_rows ) {
+				$this->data['postMessage'] = 'Success!';
+			} else {
+				$this->data['postMessage'] = 'Something went wrong!';
+			}
+
 		}
+
+		// if( in_array( $_FILES['image']['error'], [1,3,4] ) ) {
+		// 	$this->data['imageMessage'] = 'Image failed to upload';
+		// 	$totalErrors++;
+		// } elseif( !in_array( $_FILES['image']['type'], $this->acceptableImageTypes ) ) {
+		// 	$this->data['imageMessage'] = 'Must be an image (jpg, gif, png, tiff etc)';
+		// 	$totalErrors++;
+		// }
+
+		// if( $totalErrors == 0 ) {
+			
+		// 	$manager = new ImageManager(array('driver' => 'imagick'));
+
+		// 	// $image = $manager->make( $_FILES['image']['tmp_name'] )->resize(300, 200);
+
+		// 	// $image->save('img/test1.jpg');
+
+		// 	$title = $this->dbc->real_escape_string($title);
+		// 	$desc = $this->dbc->real_escape_string($desc);
+
+		// 	$userID = $_SESSION['id'];
+
+
+		// }
 	}
 
-	private function getFileExtension( $mimeType ) {
+	// private function getFileExtension( $mimeType ) {
 
-		switch($mimeType) {
+	// 	switch($mimeType) {
 
-			case 'image/png':
-				return '.png';
-			break;
+	// 		case 'image/png':
+	// 			return '.png';
+	// 		break;
 
-			case 'image/gif':
-				return '.gif';
-			break;
+	// 		case 'image/gif':
+	// 			return '.gif';
+	// 		break;
 
-			case 'image/jpeg':
-				return '.jpg';
-			break;
+	// 		case 'image/jpeg':
+	// 			return '.jpg';
+	// 		break;
 
-			case 'image/bmp':
-				return '.bmp';
-			break;
+	// 		case 'image/bmp':
+	// 			return '.bmp';
+	// 		break;
 
-			case 'image/tiff':
-				return '.tif';
-			break;
-		}
-	}
+	// 		case 'image/tiff':
+	// 			return '.tif';
+	// 		break;
+	// 	}
+	// }
 }
--- app/templates/account.php
+++ app/templates/account.php
@@ -27,8 +27,66 @@
 			      </label>
 			    </div>
 			  </div>
-			  <input type="submit" class="submit-button" class="input-group-field">
+			  <input type="submit" class="submit-button input-group-field">
 			</form>
 		</div>
 	</div>
+
+	<div class="row align-center large-5 text-center">
+		<div class="border-container">
+			<h1>Upload image</h1>
+			<form action="index.php?page=account" method="post" enctype="multipart/form-data">
+				<div>
+					<label for="title">Title: </label>
+					<input type="text" name="title" id="title" placeholder="Title" class="input-group-field">
+					<?=  isset($titleMessage) ? $titleMessage : '' ?>
+				</div>
+
+				<div>
+					<label for ="desc">Description: </label>
+					<textarea name="desc" id="desc" cols="30" rows="5" placeholder="Description" class="input-group-field"></textarea>
+					<?=  isset($descMessage) ? $descMessage : '' ?>
+				</div>
+
+				<div>
+					<label for="image">Image: </label>
+					<input type="file" name="image" id="image" class="submit-button input-group-field">
+					<?=  isset($imageMessage) ? $imageMessage : '' ?>
+				</div>
+
+				<input type="submit" name="new-upload" value="Upload image" class="submit-button input-group-field">
+				<?=  isset($postMessage) ? $postMessage : '' ?>
+			</form>
+		</div>
+	</div>
+
+	<div class="row align-center large-5 text-center">
+		<div class="border-container">
+			<h1>Create new post</h1>
+			<form action="index.php?page=account" method="post" enctype="multipart/form-data">
+				<div>
+					<label for="title">Title: </label>
+					<input type="text" name="title" id="title" placeholder="Title" class="input-group-field">
+					<?=  isset($titleMessage) ? $titleMessage : '' ?>
+				</div>
+
+				<div>
+					<label for="desc">Description: </label>
+					<textarea name="desc" id="desc" cols="30" rows="5" placeholder="Description" class="input-group-field"></textarea>
+					<?=  isset($descMessage) ? $descMessage : '' ?>
+				</div>
+
+				<div>
+					<label for="image">Image: </label>
+					<input type="file" name="image" id="image" class="input-group-field">
+					<?=  isset($imageMessage) ? $imageMessage : '' ?>
+				</div>
+
+				<input type="submit" name="new-upload" value="Submit" class="submit-button input-group-field">
+				<?=  isset($postMessage) ? $postMessage : '' ?>
+
+			</form>
+		</div>
+	</div>
+
 </div>
